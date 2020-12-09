1046a4574025659244705d5736a0620ef918957a
Red1408192 <marcored192@gmail.com>
2020-12-09 23:34:07 +0100

    interactive test

--- Main.cpp
+++ Main.cpp
@@ -5,20 +5,27 @@ using namespace std;
 
 
 int main(){
+    cout << "tile map width:\n";
+    int testnum;
+    cin >> testnum;
+    testnum = testnum%2==0? testnum : testnum - 1;
     auto start = std::chrono::steady_clock::now();
-    TileBoard tileboard = TileBoard(4800);
+    TileBoard tileboard = TileBoard(testnum);
     auto stop = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = stop-start;
-    cout << "Done in "<< diff.count() << "sec" << '\n';
+    cout << "tile map of " << testnum*(testnum/2) << " tiles \n";
+    cout << "Done in "<< diff.count() << "sec" << '\n' << '\n';
     double res;
-    for(int i = 0; i < 1001; i++){
-        int idToFind = tileboard.fullTileList[rand()%4801].id;
+    cout << "executing " << testnum/4 << " random tile search test \n";
+    for(int i = 0; i < (testnum/4)+1; i++){
+        int idToFind = tileboard.fullTileList[rand()%testnum+1].id;
         auto start = std::chrono::steady_clock::now();
         tileboard.fullTileList.GetIndex(idToFind);
         auto stop = std::chrono::steady_clock::now();
         std::chrono::duration<double> diff = stop-start;
         res += diff.count();
     }
-    cout << res/1000;
+    cout << "mean time to find a random tile:\n";
+    cout << res/(testnum/4) << "sec" << '\n';
     return 0;
 };
